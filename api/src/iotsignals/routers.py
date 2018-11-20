1656c7a396e33f123e2da0600cec17ae1dc6e0a0
Stephan Preeker <stephan@preeker.net>
2018-11-20 18:14:18 +0100

    wip API with work in progress and versioning

---
+++ api/src/iotsignals/routers.py
@@ -0,0 +1,61 @@
+"""Router configuration for multiple versions"""
+
+from django.urls import reverse
+from rest_framework import routers
+
+from iotsignals import API_VERSIONS
+from iotsignals.version import get_version
+
+
+class IOTSignalsAPIRootView(routers.APIRootView):
+    """
+    List IOT Signals API's and their related information.
+
+    These API endpoints are part of the
+    IOT Signalen Informatievoorziening Amsterdam
+
+    The code for this application (and associated web front-end)
+    is available from:
+
+    - https://github.com/Amsterdam/iotsignals
+
+    Note:
+    Most of these endpoints (will) require some form of authentication.
+    """
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+
+        # Appending the index view with API version 1 information.
+        # For now we need to mix this with
+        # the API version 0 index view.
+        v1root = request._request.build_absolute_uri(reverse('v0:api-root'))
+
+        response.data['v1'] = {
+            '_links': {
+                'self': {
+                    'href': v1root,
+                }
+            },
+            'version': get_version(API_VERSIONS['v0']),
+            'status': 'in development',
+        }
+        return response
+
+    def get_view_name(self):
+        return 'IOT Signals API'
+
+
+class IOTSignalsAPIVersion0(routers.APIRootView):
+    """Signalen API versie 0 (in development)."""
+
+    def get_view_name(self):
+        return 'Signals API Version 0'
+
+
+class IOTSignalsRouterRoot(routers.DefaultRouter):
+    APIRootView = IOTSignalsAPIRootView
+
+
+class IOTSignalsRouterVersion0(routers.DefaultRouter):
+    APIRootView = IOTSignalsAPIVersion0
---
+++ api/src/iotsignals/version.py
@@ -0,0 +1,15 @@
+
+def get_version(version: tuple = None) -> str:
+    """Return version string (x.y[.z]) from given version tuple.
+
+    Note, if no `version` tuple is given we return
+    the application version number.
+
+    :param version: version tuple or None
+    :returns: version string
+    """
+    if version is None:
+        from signals import VERSION as version
+
+    parts = 2 if version[2] == 0 else 3
+    return '.'.join(str(v) for v in version[:parts])
