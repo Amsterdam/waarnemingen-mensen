14e96f4ee0b5d53726e22031cc3dd15e5df2a319
Hielke Kramer <kramerh@gmail.com>
2019-09-18 16:36:05 +0200

    Removed the passages

--- locustfile.py
+++ locustfile.py
@@ -2,7 +2,6 @@
 This is a (much needed) load test for this repo. These are some example usages:
 
 locust --host=http://127.0.0.1:8001 --no-web -c 250 -r 25 --run-time 1m PeopleUser
-locust --host=http://127.0.0.1:8001 --no-web -c 250 -r 25 --run-time 1m CarsUser
 locust --host=http://127.0.0.1:8001 --no-web -c 250 -r 25 --run-time 30s
 """
 from uuid import uuid4
@@ -10,7 +9,6 @@ import datetime, time
 from locust import HttpLocust, TaskSet, task
 
 
-PASSAGE_ENDPOINT_URL = "/v0/milieuzone/passage/"
 PEOPLE_MEASUREMENT_ENDPOINT_URL = "/v0/people/measurement/"
 
 
@@ -21,102 +19,92 @@ def get_dt_with_tz_info():
     return datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
 
 
-def create_message(type_):
-    if type_ == 'people':
-        return {
-            "data": {
-                "id": str(uuid4()),
-                "density": "0.0",
-                "sensortype": "countingcamera",
-                "latitude": "52.3709",
-                "count": "6.0",
-                "sensor": "GKS-1-4",
-        
-                "version": "1",
-                "speed": "0.0",
-                "timestamp": get_dt_with_tz_info(),
-                "longitude": "4.89175"
-            },
-            "details": [
-                {
-                    "count": "2",
-                    "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
-                    "direction": "down"
-                },
-                {
-                    "count": "4",
-                    "id": "a70b424f-4144-437b-a990-cbac421a4830",
-                    "direction": "down"
-                }
-            ]
-        }
-    elif type_ == 'cars':
-        return {
+def create_message():
+    return {
+        "data": {
             "id": str(uuid4()),
-            "passage_at": get_dt_with_tz_info(),
-            "created_at": get_dt_with_tz_info(),
+            "density": "0.0",
+            "sensortype": "countingcamera",
+            "latitude": "52.3709",
+            "count": "6.0",
+            "sensor": "TEST-SENSOR",
+
             "version": "1",
-            "straat": None,
-            "rijrichting": 1,
-            "rijstrook": 2,
-            "camera_id": "00856ef3-c6f5-4194-9531-a3267839674a",
-            "camera_naam": "Muntbergweg (s111) nabij afrit (A9) uit oost - Rijstrook 2",
-            "camera_kijkrichting": 337.5,
-            "camera_locatie": {
-                "type": "Point",
-                "coordinates": [
-                    4.945936,
-                    52.301221
-                ]
+            "speed": "0.0",
+            "timestamp": get_dt_with_tz_info(),
+            "longitude": "4.89175"
+        },
+        "details": [
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
             },
-            "kenteken_land": "NL",
-            "kenteken_nummer_betrouwbaarheid": 990,
-            "kenteken_land_betrouwbaarheid": 0,
-            "kenteken_karakters_betrouwbaarheid": None,
-            "indicatie_snelheid": None,
-            "automatisch_verwerkbaar": None,
-            "voertuig_soort": "Personenauto",
-            "merk": "SPYKER",
-            "inrichting": "stationwagen",
-            "datum_eerste_toelating": "2001-01-01",
-            "datum_tenaamstelling": "2001-01-02",
-            "toegestane_maximum_massa_voertuig": 1234,
-            "europese_voertuigcategorie": "M1",
-            "europese_voertuigcategorie_toevoeging": None,
-            "taxi_indicator": False,
-            "maximale_constructie_snelheid_bromsnorfiets": None,
-            "brandstoffen": [
-                {
-                    "volgnr": 1,
-                    "brandstof": "Benzine",
-                    "euroklasse": "Euro 3"
-                }
-            ],
-            "extra_data": None,
-            "diesel": None,
-            "gasoline": None,
-            "electric": None,
-            "versit_klasse": "LPABEUR3"
-        }
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "2",
+                "id": "315fc896-82dd-46ee-ac91-31e1a1b1807a",
+                "direction": "down"
+            },
+            {
+                "count": "4",
+                "id": "a70b424f-4144-437b-a990-cbac421a4830",
+                "direction": "down"
+            }
+        ]
+    }
 
 
 class PeopleBehaviour(TaskSet):
     @task(1)
     def post_people(self):
-        self.client.post(PEOPLE_MEASUREMENT_ENDPOINT_URL, json=create_message('people'))
-
-
-class CarsBehaviour(TaskSet):
-    @task(1)
-    def post_cars(self):
-        self.client.post(PASSAGE_ENDPOINT_URL, json=create_message('cars'))
+        self.client.post(PEOPLE_MEASUREMENT_ENDPOINT_URL, json=create_message())
 
 
 class PeopleUser(HttpLocust):
     task_set = PeopleBehaviour
-    weight = 1
-
-
-class CarsUser(HttpLocust):
-    task_set = CarsBehaviour
     weight = 1
