60cbf1fd1e00abacd4ca485a878fd6bda1a78aab
Hielke Kramer <kramerh@gmail.com>
2020-03-30 16:15:30 +0200

    Added a materialized view of the cmsa_15min view

---
+++ api/src/peoplemeasurement/migrations/0005_cmsa_15min_materialized_view_20200304_1633.py
@@ -0,0 +1,24 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('peoplemeasurement', '0004_cmsa_15min_view_20190926_1130'),
+    ]
+
+    _VIEW_NAME = "cmsa_15min_materialized"
+
+    sql = f"""
+CREATE MATERIALIZED VIEW {_VIEW_NAME} AS
+SELECT * FROM cmsa_15min;
+"""
+
+    reverse_sql = f"drop view if exists {_VIEW_NAME};"
+
+    operations = [
+        migrations.RunSQL(
+            sql=sql,
+            reverse_sql=reverse_sql
+        ),
+    ]
