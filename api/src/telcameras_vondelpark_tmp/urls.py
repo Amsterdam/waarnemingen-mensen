e34efbd9f92e96debdf310dd80ee291cfcaf5ecb
Hielke Kramer <kramerh@gmail.com>
2020-07-16 20:18:50 +0200

    Remove vondelpark table from DB

--- api/src/telcameras_vondelpark_tmp/urls.py
+++ api/src/telcameras_vondelpark_tmp/urls.py
@@ -1,9 +1,5 @@
-from django.conf.urls import include, url
 from rest_framework import routers
 
-from . import views
-
 router = routers.DefaultRouter()
-router.register('', views.JsonDumpViewSet)
 
 urlpatterns = router.urls
