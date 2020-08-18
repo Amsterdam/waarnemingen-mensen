80bb88bd0f32e549824e5d062fa93c7b29915678
Hielke Kramer <kramerh@gmail.com>
2020-08-18 23:45:04 +0200

    Added 15min aggregate endpoint

---
+++ api/src/peoplemeasurement/queries.py
@@ -0,0 +1,97 @@
+def get_today_15min_aggregation_sql(datestr):
+    return f"""
+with RawData as(
+    select
+        sensor,
+        timestamp,
+        coalesce(avg((detail_elems ->> 'count')::integer) filter (where detail_elems ->> 'direction' = 'down'), 0) + coalesce(avg((detail_elems ->> 'count')::integer) filter (where detail_elems ->> 'direction' = 'up'), 0) as total_count,
+        coalesce(avg((detail_elems ->> 'count')::integer) filter (where detail_elems ->> 'direction' = 'down'), 0) as count_down,
+        coalesce(avg((detail_elems ->> 'count')::integer) filter (where detail_elems ->> 'direction' = 'up'), 0) as count_up,
+        avg((detail_elems ->> 'count')::numeric) filter (where detail_elems ->> 'direction' = 'density') as density_avg,
+        avg((detail_elems ->> 'count')::numeric) filter (where detail_elems ->> 'direction' = 'speed') as speed_avg
+    from peoplemeasurement_peoplemeasurement, jsonb_array_elements(details) detail_elems
+    where timestamp > '{datestr}'
+    group by sensor, timestamp
+    order by sensor, timestamp
+),
+AggregatedByQuarter as(
+    select
+        sensor,
+        date_trunc('hour', timestamp) + date_part('minute', timestamp)::int / 15 * interval '15 min' as timestamp_rounded,
+        round((avg(total_count) * 15), 0) as total_count,
+        round((avg(count_down) * 15), 0) as count_down,
+        round((avg(count_up) * 15), 0) as count_up,
+        avg(density_avg) as density_avg,
+        avg(speed_avg) as speed_avg,
+        count(*) as BasedOnXMessages
+    from RawData
+    where total_count != 0
+    group by sensor, timestamp_rounded
+    order by sensor, timestamp_rounded
+),
+Percentiles as(
+    select
+        sensor,
+        extract(dow from timestamp_rounded)::int as dayofweek,
+        timestamp_rounded::time as CastedTimeStamp,
+        percentile_disc(0.1) within group (order by total_count) as total_count_p10,
+        percentile_disc(0.2) within group (order by total_count) as total_count_p20,
+        percentile_disc(0.5) within group (order by total_count) as total_count_p50,
+        percentile_disc(0.8) within group (order by total_count) as total_count_p80,
+        percentile_disc(0.9) within group (order by total_count) as total_count_p90,
+        percentile_disc(0.1) within group (order by count_down) as count_down_p10,
+        percentile_disc(0.2) within group (order by count_down) as count_down_p20,
+        percentile_disc(0.5) within group (order by count_down) as count_down_p50,
+        percentile_disc(0.8) within group (order by count_down) as count_down_p80,
+        percentile_disc(0.9) within group (order by count_down) as count_down_p90,
+        percentile_disc(0.1) within group (order by count_up) as count_up_p10,
+        percentile_disc(0.2) within group (order by count_up) as count_up_p20,
+        percentile_disc(0.5) within group (order by count_up) as count_up_p50,
+        percentile_disc(0.8) within group (order by count_up) as count_up_p80,
+        percentile_disc(0.9) within group (order by count_up) as count_up_p90,
+        percentile_disc(0.2) within group (order by density_avg) as density_avg_p20,
+        percentile_disc(0.5) within group (order by density_avg) as density_avg_p50,
+        percentile_disc(0.8) within group (order by density_avg) as density_avg_p80,
+        percentile_disc(0.2) within group (order by speed_avg) as speed_avg_p20,
+        percentile_disc(0.5) within group (order by speed_avg) as speed_avg_p50,
+        percentile_disc(0.8) within group (order by speed_avg) as speed_avg_p80
+    from AggregatedByQuarter
+    where timestamp_rounded >= (select now() - interval '1 year')
+    group by
+        sensor,
+        extract(dow from timestamp_rounded),
+        timestamp_rounded::time
+)
+select
+    aq.*,
+    total_count_p10,
+    total_count_p20,
+    total_count_p50,
+    total_count_p80,
+    total_count_p90,
+    count_down_p10,
+    count_down_p20,
+    count_down_p50,
+    count_down_p80,
+    count_down_p90,
+    count_up_p10,
+    count_up_p20,
+    count_up_p50,
+    count_up_p80,
+    count_up_p90,
+    density_avg_p20,
+    density_avg_p50,
+    density_avg_p80,
+    speed_avg_p20,
+    speed_avg_p50,
+    speed_avg_p80
+from AggregatedByQuarter aq
+left join percentiles p on
+    aq.sensor = p.sensor
+    and extract(dow from aq.timestamp_rounded) = p.DayOfWeek
+    and aq.timestamp_rounded::time = p.CastedTimeStamp
+order by
+    sensor,
+    timestamp_rounded
+;
+"""
