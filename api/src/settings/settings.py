2bf4a6ff85e5be184988a5aa8f3481b647be9b8e
Hielke Kramer <kramerh@gmail.com>
2020-08-19 06:59:28 +0200

    Removed settings_common.py

--- api/src/settings/settings.py
+++ api/src/settings/settings.py
@@ -11,13 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-
-from settings.settings_common import *  # noqa F403
-from settings.settings_common import REST_FRAMEWORK  # noqa
-from settings.settings_common import INSTALLED_APPS
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -30,11 +27,29 @@ AUTHORIZATION_TOKEN = os.environ['AUTHORIZATION_TOKEN']
 
 
 ALLOWED_HOSTS = ['*']
+X_FRAME_OPTIONS = 'ALLOW-FROM *'
+INTERNAL_IPS = ('127.0.0.1', '0.0.0.0')
 
 # Application definition
 HEALTH_MODEL = 'peoplemeasurement.PeopleMeasurement'
 
-INSTALLED_APPS += [
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "django.contrib.staticfiles",
+    "django_filters",
+    "django_extensions",
+    "django.contrib.gis",
+    "datapunt_api",
+    "rest_framework",
+    "rest_framework_gis",
     'health',
     'datetimeutc',
     'settings',
@@ -78,8 +93,6 @@ DATABASES = {
 }
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.1/topics/i18n/
 TIME_ZONE = 'Europe/Amsterdam'
 USE_TZ = True
 
@@ -87,12 +100,141 @@ LANGUAGE_CODE = 'en-us'
 USE_I18N = True
 USE_L10N = True
 
+DUMP_DIR = "mks-dump"
+TESTING = len(sys.argv) > 1 and sys.argv[1] == "test"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/mensen/static/'
 STATIC_ROOT = '/static/'
+
+if DEBUG:
+    INSTALLED_APPS += ('debug_toolbar',)
+    MIDDLEWARE += (
+        # 'corsheaders.middleware.CorsMiddleware',
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+    CORS_ORIGIN_ALLOW_ALL = True
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+    ]
+
+
+REST_FRAMEWORK = dict(
+    PAGE_SIZE=20,
+    MAX_PAGINATE_BY=100,
+    UNAUTHENTICATED_USER={},
+    UNAUTHENTICATED_TOKEN={},
+    DEFAULT_AUTHENTICATION_CLASSES=(
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'contrib.rest_framework.authentication.SimpleTokenAuthentication',
+    ),
+    DEFAULT_PAGINATION_CLASS=("datapunt_api.pagination.HALPagination",),
+    DEFAULT_RENDERER_CLASSES=(
+        "rest_framework.renderers.JSONRenderer",
+        "datapunt_api.renderers.PaginatedCSVRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+        # must be lowest!
+        "rest_framework_xml.renderers.XMLRenderer",
+    ),
+    DEFAULT_FILTER_BACKENDS=(
+        # 'rest_framework.filters.SearchFilter',
+        # 'rest_framework.filters.OrderingFilter',
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ),
+    DEFAULT_VERSIONING_CLASS='rest_framework.versioning.NamespaceVersioning',
+    COERCE_DECIMAL_TO_STRING=True,
+)
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "console"
+        }
+    },
+    "root": {"level": "DEBUG", "handlers": ["console"]},
+    "loggers": {
+        "django.db": {
+            "handlers": ["console"],
+            "level": "ERROR"
+        },
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR"
+        },
+        # Debug all batch jobs
+        "doc": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False
+        },
+        "index": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False
+        },
+        "search": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False
+        },
+        "elasticsearch": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False
+        },
+        "urllib3": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False
+        },
+        "factory.containers": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False
+        },
+        "factory.generate": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False
+        },
+        "requests.packages.urllib3.connectionpool": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False
+        },
+        # Log all unhandled exceptions
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False
+        },
+    },
+}
 
 SENTRY_DSN = os.getenv('SENTRY_DSN')
 if SENTRY_DSN:
