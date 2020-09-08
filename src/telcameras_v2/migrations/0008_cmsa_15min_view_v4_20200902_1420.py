a5636e5eff29f68c07ae2af9a44c4643f78705a9
Hielke Kramer <kramerh@gmail.com>
2020-09-08 15:11:25 +0200

    The v4 view is adjusted.
    Since it's only been deployed on acceptance I'll manually remove it there before deploying it.

--- src/telcameras_v2/migrations/0008_cmsa_15min_view_v4_20200902_1420.py
+++ src/telcameras_v2/migrations/0008_cmsa_15min_view_v4_20200902_1420.py
@@ -12,140 +12,207 @@ class Migration(migrations.Migration):
     # NOTE: the regex in this query causes a DeprecationWarning: invalid escape sequence
     # For this reason we use a rawstring for that part of the query
     sql = f"CREATE VIEW {_VIEW_NAME} AS" + r"""
-with rawdata as(
-    with observatie_snelheid as(
-        with observatie_persoon as (
-            with observatie_persoon_3d as (     --bepaal tijdsduur op basis van de geom data
-                select 
-                    observation_id,             --dit is alleen bij 3D camera's gevuld
-                    speed,                      --negeer person records met onbepaalde speed null
-                    (array_replace(regexp_matches(geom,'([0-9.]*)\)'),'','0'))[1]::numeric-
-                        (array_replace(regexp_matches(geom,'\(([0-9.-]*)'),'','0'))[1]::numeric as tijd
-                from telcameras_v2_personaggregate
-                where speed is not null
-                and geom is not null
-                and geom != ''
-            ),
-             observatie_persoon_2d as (  --voor records uit 2D camera's die geen geom data hebben
-                select
-                    observation_id,
-                    speed, 
-                    1 as tijd            -- bereken de gemiddelde snelheid op basis van een ongewogen gemiddelde
-                from telcameras_v2_personaggregate 
-                where speed is not null 
-                and (geom is null or geom = '')
-            )
-            select * from observatie_persoon_3d union all select * from observatie_persoon_2d 
+WITH rawdata AS (
+    WITH v2_observatie_snelheid AS (
+        WITH v2_observatie_persoon AS (
+            WITH 
+                v2_observatie_persoon_3d AS (
+                    SELECT 
+                        observation_id,
+                        speed,
+                        (array_replace(regexp_matches(geom::text, '([0-9.]*)\)'::text), ''::text, '0'::text))[1]::numeric
+                            - (array_replace(regexp_matches(geom::text, '\(([0-9.-]*)'::text), ''::text, '0'::text))[1]::numeric AS tijd
+                    FROM telcameras_v2_personaggregate
+                    WHERE speed IS NOT NULL
+                    AND geom IS NOT NULL
+                    AND geom::text <> ''::text
+                ), 
+                v2_observatie_persoon_2d AS (
+                    SELECT
+                        observation_id,
+                        speed,
+                        1 AS tijd
+                    FROM telcameras_v2_personaggregate
+                    WHERE speed IS NOT NULL
+                    AND (geom IS NULL OR geom::text = ''::text)
+                )
+                SELECT 
+                    v2_observatie_persoon_3d.observation_id,
+                    v2_observatie_persoon_3d.speed,
+                    v2_observatie_persoon_3d.tijd
+                FROM v2_observatie_persoon_3d UNION ALL SELECT 
+                    v2_observatie_persoon_2d.observation_id,
+                    v2_observatie_persoon_2d.speed,
+                    v2_observatie_persoon_2d.tijd
+                FROM v2_observatie_persoon_2d
         )
-        select 
-            observation_id, 
+        SELECT
+            v2_observatie_persoon.observation_id,
             CASE
-                when sum(tijd) is not null and sum(tijd) != 0 then round((sum(speed*tijd)/sum(tijd))::numeric,2)
-                ELSE null
-            END as speed_avg
-        from observatie_persoon 
-        group by observation_id
-    ),
-    countaggregate_zone_count as (
-        select 
+                WHEN sum(v2_observatie_persoon.tijd) IS NOT NULL 
+                AND sum(v2_observatie_persoon.tijd) <> 0::numeric 
+                THEN 
+                    round((sum(v2_observatie_persoon.speed * v2_observatie_persoon.tijd::double precision) / sum(v2_observatie_persoon.tijd)::double precision)::numeric, 2)
+                ELSE NULL::numeric
+            END AS speed_avg
+        FROM v2_observatie_persoon
+        GROUP BY v2_observatie_persoon.observation_id
+    ), 
+    v2_countaggregate_zone_count AS (
+        SELECT
             observation_id,
-            max(azimuth) as azimuth, 
-            max(count_in) as count_in, 
-            max(count_out) as count_out,
-            max(area) as area, 
-            max(count) as count 
-        from telcameras_v2_countaggregate 
-        group by observation_id)
-    select 
-        o.sensor,
-        o.timestamp_start as timestamp,
-        COALESCE((count_in+count_out),0) as total_count, 
-        COALESCE(count_in,0) as count_up, COALESCE(count_out,0) as count_down, 
-        CASE
-            when area is not null and area != 0 and count is not null and count>0 then count/area
-            ELSE null
-        END as density_avg, 
-    s.speed_avg   
-    from telcameras_v2_observation o
-    LEFT JOIN observatie_snelheid s on o.id=s.observation_id
-    LEFT JOIN countaggregate_zone_count c on o.id=c.observation_id
+            max(azimuth) AS azimuth,
+            max(count_in) AS count_in,
+            max(count_out) AS count_out,
+            max(area) AS area,
+            max(count) AS count
+        FROM telcameras_v2_countaggregate
+        GROUP BY observation_id
+    ),
+    v1_data_uniek AS (
+        SELECT max(id::text) AS idt
+        FROM peoplemeasurement_peoplemeasurement
+        GROUP BY sensor, "timestamp"
+    ),
+    v1_data_sel AS (
+        SELECT
+            dp.sensor,
+            dp."timestamp",
+            dp.details
+        FROM peoplemeasurement_peoplemeasurement dp
+        JOIN v1_data_uniek csdu ON dp.id::text = csdu.idt
+    ),
+    v1_data AS (
+        SELECT
+            v1_data_sel.sensor||'_v1' AS sensor,
+            v1_data_sel."timestamp",
+            COALESCE(sum((detail_elems.value ->> 'count'::text)::integer) FILTER (WHERE (detail_elems.value ->> 'direction'::text) = 'down'::text), 0::bigint) + COALESCE(sum((detail_elems.value ->> 'count'::text)::integer) FILTER (WHERE (detail_elems.value ->> 'direction'::text) = 'up'::text), 0::bigint) AS total_count,
+            COALESCE(sum((detail_elems.value ->> 'count'::text)::integer) FILTER (WHERE (detail_elems.value ->> 'direction'::text) = 'down'::text), 0::bigint) AS count_down,
+            COALESCE(sum((detail_elems.value ->> 'count'::text)::integer) FILTER (WHERE (detail_elems.value ->> 'direction'::text) = 'up'::text), 0::bigint) AS count_up,
+            avg((detail_elems.value ->> 'count'::text)::numeric) FILTER (WHERE (detail_elems.value ->> 'direction'::text) = 'density'::text) AS density_avg,
+            avg((detail_elems.value ->> 'count'::text)::numeric) FILTER (WHERE (detail_elems.value ->> 'direction'::text) = 'speed'::text) AS speed_avg
+        FROM v1_data_sel,
+        LATERAL jsonb_array_elements(v1_data_sel.details) detail_elems(value)
+        GROUP BY v1_data_sel.sensor, v1_data_sel."timestamp"
+        ORDER BY v1_data_sel.sensor, v1_data_sel."timestamp"
+    ),
+    v2_data AS (
+        SELECT
+            o.sensor,
+            o.timestamp_start AS "timestamp",
+            COALESCE((c.count_in + c.count_out)::integer, 0) AS total_count,
+            COALESCE(c.count_in::integer, 0) AS count_up,
+            COALESCE(c.count_out::integer, 0) AS count_down,
+            CASE
+                WHEN c.area IS NOT NULL AND c.area <> 0::double precision AND c.count IS NOT NULL AND c.count > 0 THEN c.count::double precision / c.area
+                ELSE NULL::double precision
+            END AS density_avg,
+            s.speed_avg
+        FROM telcameras_v2_observation o
+        LEFT JOIN v2_observatie_snelheid s ON o.id = s.observation_id
+        LEFT JOIN v2_countaggregate_zone_count c ON o.id = c.observation_id
+    )
+    SELECT
+        v1_data.sensor,
+        v1_data."timestamp",
+        v1_data.total_count,
+        v1_data.count_down,
+        v1_data.count_up,
+        v1_data.density_avg,
+        v1_data.speed_avg
+    FROM v1_data UNION ALL SELECT
+        v2_data.sensor,
+        v2_data."timestamp",
+        v2_data.total_count,
+        v2_data.count_up,
+        v2_data.count_down,
+        v2_data.density_avg,
+        v2_data.speed_avg
+    FROM v2_data
 ),
-AggregatedByQuarter as(
-    select
-        sensor,
-        date_trunc('hour', timestamp) + date_part('minute', timestamp)::int / 15 * interval '15 min' as timestamp_rounded,
-        round((avg(total_count) * 15), 0) as total_count,
-        round((avg(count_down) * 15), 0) as count_down,
-        round((avg(count_up) * 15), 0) as count_up,
-        avg(density_avg) as density_avg,
-        avg(speed_avg) as speed_avg,
-        count(*) as BasedOnXMessages
-    from RawData
-    group by sensor, timestamp_rounded
-    order by sensor, timestamp_rounded),
-Percentiles as(
-    select
-        sensor,
-        extract(dow from timestamp_rounded)::int as dayofweek,
-        timestamp_rounded::time as CastedTimeStamp,
-        percentile_disc(0.1) within group (order by total_count) as total_count_p10,
-        percentile_disc(0.2) within group (order by total_count) as total_count_p20,
-        percentile_disc(0.5) within group (order by total_count) as total_count_p50,
-        percentile_disc(0.8) within group (order by total_count) as total_count_p80,
-        percentile_disc(0.9) within group (order by total_count) as total_count_p90,
-        percentile_disc(0.1) within group (order by count_down) as count_down_p10,
-        percentile_disc(0.2) within group (order by count_down) as count_down_p20,
-        percentile_disc(0.5) within group (order by count_down) as count_down_p50,
-        percentile_disc(0.8) within group (order by count_down) as count_down_p80,
-        percentile_disc(0.9) within group (order by count_down) as count_down_p90,
-        percentile_disc(0.1) within group (order by count_up) as count_up_p10,
-        percentile_disc(0.2) within group (order by count_up) as count_up_p20,
-        percentile_disc(0.5) within group (order by count_up) as count_up_p50,
-        percentile_disc(0.8) within group (order by count_up) as count_up_p80,
-        percentile_disc(0.9) within group (order by count_up) as count_up_p90,
-        percentile_disc(0.2) within group (order by density_avg) as density_avg_p20,
-        percentile_disc(0.5) within group (order by density_avg) as density_avg_p50,
-        percentile_disc(0.8) within group (order by density_avg) as density_avg_p80,
-        percentile_disc(0.2) within group (order by speed_avg) as speed_avg_p20,
-        percentile_disc(0.5) within group (order by speed_avg) as speed_avg_p50,
-        percentile_disc(0.8) within group (order by speed_avg) as speed_avg_p80
-    from AggregatedByQuarter
-    where timestamp_rounded >= (select now() - interval '1 year')
-    group by
-        sensor,
-        extract(dow from timestamp_rounded),
-        timestamp_rounded::time)
-select
-    aq.*,
-    total_count_p10,
-    total_count_p20,
-    total_count_p50,
-    total_count_p80,
-    total_count_p90,
-    count_down_p10,
-    count_down_p20,
-    count_down_p50,
-    count_down_p80,
-    count_down_p90,
-    count_up_p10,
-    count_up_p20,
-    count_up_p50,
-    count_up_p80,
-    count_up_p90,
-    density_avg_p20,
-    density_avg_p50,
-    density_avg_p80,
-    speed_avg_p20,
-    speed_avg_p50,
-    speed_avg_p80
-from AggregatedByQuarter aq
-left join percentiles p on
-    aq.sensor = p.sensor
-    and extract(dow from aq.timestamp_rounded) = p.DayOfWeek
-    and aq.timestamp_rounded::time = p.CastedTimeStamp
-order by
-    sensor,
-    timestamp_rounded
+aggregatedbyquarter AS (
+    SELECT
+        rawdata.sensor,
+        date_trunc('hour'::text, rawdata."timestamp") + (date_part('minute'::text, rawdata."timestamp")::integer / 15)::double precision * '00:15:00'::interval AS timestamp_rounded,
+        round(avg(rawdata.total_count) * 15::numeric, 0) AS total_count,
+        round(avg(rawdata.count_down) * 15::numeric, 0) AS count_down,
+        round(avg(rawdata.count_up) * 15::numeric, 0) AS count_up,
+        avg(rawdata.density_avg) AS density_avg,
+        avg(rawdata.speed_avg) AS speed_avg,
+        count(*) AS basedonxmessages
+    FROM rawdata
+    GROUP BY rawdata.sensor, (date_trunc('hour'::text, rawdata."timestamp") + (date_part('minute'::text, rawdata."timestamp")::integer / 15)::double precision * '00:15:00'::interval)
+    ORDER BY rawdata.sensor, (date_trunc('hour'::text, rawdata."timestamp") + (date_part('minute'::text, rawdata."timestamp")::integer / 15)::double precision * '00:15:00'::interval)
+),
+percentiles AS (
+    SELECT
+        aggregatedbyquarter.sensor,
+        date_part('dow'::text, aggregatedbyquarter.timestamp_rounded)::integer AS dayofweek,
+        aggregatedbyquarter.timestamp_rounded::time without time zone AS castedtimestamp,
+        percentile_disc(0.1::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.total_count) AS total_count_p10,
+        percentile_disc(0.2::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.total_count) AS total_count_p20,
+        percentile_disc(0.5::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.total_count) AS total_count_p50,
+        percentile_disc(0.8::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.total_count) AS total_count_p80,
+        percentile_disc(0.9::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.total_count) AS total_count_p90,
+        percentile_disc(0.1::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_down) AS count_down_p10,
+        percentile_disc(0.2::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_down) AS count_down_p20,
+        percentile_disc(0.5::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_down) AS count_down_p50,
+        percentile_disc(0.8::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_down) AS count_down_p80,
+        percentile_disc(0.9::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_down) AS count_down_p90,
+        percentile_disc(0.1::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_up) AS count_up_p10,
+        percentile_disc(0.2::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_up) AS count_up_p20,
+        percentile_disc(0.5::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_up) AS count_up_p50,
+        percentile_disc(0.8::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_up) AS count_up_p80,
+        percentile_disc(0.9::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.count_up) AS count_up_p90,
+        percentile_disc(0.2::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.density_avg) AS density_avg_p20,
+        percentile_disc(0.5::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.density_avg) AS density_avg_p50,
+        percentile_disc(0.8::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.density_avg) AS density_avg_p80,
+        percentile_disc(0.2::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.speed_avg) AS speed_avg_p20,
+        percentile_disc(0.5::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.speed_avg) AS speed_avg_p50,
+        percentile_disc(0.8::double precision) WITHIN GROUP (ORDER BY aggregatedbyquarter.speed_avg) AS speed_avg_p80
+    FROM aggregatedbyquarter
+    WHERE aggregatedbyquarter.timestamp_rounded >= (( SELECT now() - '1 year'::interval))
+    GROUP BY
+        aggregatedbyquarter.sensor, (date_part('dow'::text, aggregatedbyquarter.timestamp_rounded)),
+        (aggregatedbyquarter.timestamp_rounded::time without time zone)
+)
+SELECT
+    aq.sensor,
+    aq.timestamp_rounded,
+    aq.total_count,
+    aq.count_down,
+    aq.count_up,
+    aq.density_avg,
+    aq.speed_avg,
+    aq.basedonxmessages,
+    p.total_count_p10,
+    p.total_count_p20,
+    p.total_count_p50,
+    p.total_count_p80,
+    p.total_count_p90,
+    p.count_down_p10,
+    p.count_down_p20,
+    p.count_down_p50,
+    p.count_down_p80,
+    p.count_down_p90,
+    p.count_up_p10,
+    p.count_up_p20,
+    p.count_up_p50,
+    p.count_up_p80,
+    p.count_up_p90,
+    p.density_avg_p20,
+    p.density_avg_p50,
+    p.density_avg_p80,
+    p.speed_avg_p20,
+    p.speed_avg_p50,
+    p.speed_avg_p80
+FROM aggregatedbyquarter aq
+LEFT JOIN percentiles p ON aq.sensor::text = p.sensor::TEXT
+    AND date_part('dow'::text, aq.timestamp_rounded) = p.dayofweek::double PRECISION
+    AND aq.timestamp_rounded::time without time zone = p.castedtimestamp
+ORDER BY
+    aq.sensor,
+    aq.timestamp_rounded
 ;
 """
 
