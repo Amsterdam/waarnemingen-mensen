54eeda7fe9388ff202152a8798dd4d9f36d271b3
Jeffrey Klardie <j.klardie@amsterdam.nl>
2020-10-08 14:12:03 +0200

    Added gebied, imported_at and updated_at to Sensors

--- src/peoplemeasurement/models.py
+++ src/peoplemeasurement/models.py
@@ -34,6 +34,9 @@ class Sensors(models.Model):
     location_name = models.CharField(max_length=255, null=True)
     width = models.FloatField(null=True)
     gebiedstype = models.CharField(max_length=255, null=True)
+    gebied = models.CharField(max_length=255, null=True)
+    imported_at = models.DateTimeField(null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
 
 
 class Servicelevel(models.Model):
