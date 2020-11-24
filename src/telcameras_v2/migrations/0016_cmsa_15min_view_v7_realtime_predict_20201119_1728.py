bdda5b7f97c63b0aa22063ee6704b3b17ff9c7c5
Hielke <kramerh@gmail.com>
2020-11-24 11:30:34 +0100

    Re-added the timescale migrations for deployment to production
    The message below is mostly the same as in the previous commit, but for future readers I added this explanation here as well.
    
    In order to do the timescale migration, the receiving of the data should first be moved to the new fault tolerant endpoint. There have been adjustments to this system after the timescale migrations on acc though. For that reason this commit removes the migrations to timescale (and those thereafter). That way we can deploy this to production. Add the new fault tolerant endpoint. And then re-add these timescale migrations again once the data stream has been moved to the fault tolerant endpoint.

---
+++ src/telcameras_v2/migrations/0016_cmsa_15min_view_v7_realtime_predict_20201119_1728.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+from telcameras_v2.view_definitions import get_view_strings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('telcameras_v2', '0015_cmsa_15min_view_v7_20201117_1514'),
+    ]
+
+    _VIEW_NAME = "cmsa_15min_view_v7_realtime_predict"
+    _view_strings = get_view_strings(_VIEW_NAME)
+
+    operations = [
+        migrations.RunSQL(
+            sql=_view_strings['sql'],
+            reverse_sql=_view_strings['reverse_sql']
+        ),
+    ]
