0ed7e34faf2ab6d3f3d11482e855e321401a0bbf
Hielke <kramerh@gmail.com>
2021-02-08 17:13:34 +0100

    Drop old (pre-timescale) tables

---
+++ src/telcameras_v2/migrations/0018_drop_old_tables_20200208_1555.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+TABLES = [
+    'telcameras_v2_countaggregate',
+    'telcameras_v2_personaggregate',
+    'telcameras_v2_observation',
+]
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('telcameras_v2', '0017_cmsa_15min_view_v7_again_20201201_1515'),
+    ]
+
+    # Remove the old tables which are still present after the migration to timescaledb
+    operations = []
+    for table in TABLES:
+        operations.append(migrations.RunSQL(sql=f"DROP TABLE {table}_old;"))
