e5bfc9fb6597b6184648c67d9fd0a2640ef6309d
Hielke <kramerh@gmail.com>
2021-02-11 13:49:11 +0100

    Add migration to redeploy cmsa_15min_view_v8

---
+++ src/telcameras_v2/migrations/0021_redeploy_cmsa_15min_view_v8_20210211_1344.py
@@ -0,0 +1,35 @@
+from django.db import migrations
+
+from telcameras_v2.view_definitions import get_view_strings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('telcameras_v2', '0020_cmsa_15min_view_v8_20210209_2016'),
+    ]
+
+    _VIEW_NAME = "cmsa_15min_view_v8"
+    _view_strings = get_view_strings(_VIEW_NAME)
+
+    operations = [
+        # First remove the views
+        migrations.RunSQL(
+            sql=_view_strings['reverse_sql_materialized'],
+            reverse_sql=_view_strings['sql_materialized'],
+        ),
+        migrations.RunSQL(
+            sql=_view_strings['reverse_sql'],
+            reverse_sql=_view_strings['sql'],
+        ),
+
+        # And then redeploy it again
+        migrations.RunSQL(
+            sql=_view_strings['sql'],
+            reverse_sql=_view_strings['reverse_sql']
+        ),
+        migrations.RunSQL(
+            sql=_view_strings['sql_materialized'],
+            reverse_sql=_view_strings['reverse_sql_materialized']
+        ),
+    ]
