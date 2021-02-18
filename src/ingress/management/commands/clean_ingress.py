811cc3963e3eeb148d7a531aeed9051f30357ef3
Jeffrey Klardie <j.klardie@amsterdam.nl>
2021-02-18 16:05:45 +0100

    Removed local ingress app

--- src/ingress/management/commands/clean_ingress.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from ingress.models import IngressQueue
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        result = IngressQueue.objects.filter(parse_succeeded__isnull=False).delete()
-        self.stdout.write(str(result[0]))
