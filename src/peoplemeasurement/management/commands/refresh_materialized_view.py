29e7b9e9c6aeb06745eba957e40e238ece38ea9f
Hielke <kramerh@gmail.com>
2021-03-03 12:23:22 +0100

    Added optional refresh flag to refresh_materialized_view command

--- src/peoplemeasurement/management/commands/refresh_materialized_view.py
+++ src/peoplemeasurement/management/commands/refresh_materialized_view.py
@@ -11,11 +11,14 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('view_name', type=str)
+        parser.add_argument('--concurrently', default=False, action='store_true')
 
     def handle(self, *args, **options):
         view_name = options['view_name']
+        concurrently = 'CONCURRENTLY' if options['concurrently'] else ''
 
         log.info(f"Start refreshing the {view_name}")
+        refresh_query = f"REFRESH MATERIALIZED VIEW {concurrently} {view_name};"
         with connection.cursor() as cursor:
-            cursor.execute(f"REFRESH MATERIALIZED VIEW {view_name};")
+            cursor.execute(refresh_query)
         log.info(f"Done refreshing the {view_name}")
