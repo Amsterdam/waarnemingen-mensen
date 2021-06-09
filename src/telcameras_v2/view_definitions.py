02eea483d82bc802100f1e81cf978f3cb70f4d98
eg2ie <coen.egberink@sigmadataconsulting.nl>
2021-06-09 13:20:14 +0200

    use field area_count (instead of total_count) also for sensors starting with 'GABW'

--- src/telcameras_v2/view_definitions.py
+++ src/telcameras_v2/view_definitions.py
@@ -1499,7 +1499,7 @@ VIEW_STRINGS = {
               sel3.sensor
             , sel3.timestamp_rounded
             , case      
-                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB')         -- This filter applies to zone sensors for wich only the area_count is filled
+                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB', 'GABW')         -- This filter applies to zone sensors for wich only the area_count is filled
                 then coalesce(oc.area_count::integer, 0)
                 else coalesce(oc.total_count::integer, 0)
               end                                   as total_count
@@ -2046,7 +2046,7 @@ VIEW_STRINGS = {
               sel3.sensor
             , sel3.timestamp_rounded
             , case
-                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB')     -- This filter applies to zone sensors for wich only the area_count is filled
+                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB', 'GABW')     -- This filter applies to zone sensors for wich only the area_count is filled
                 then coalesce(oc.area_count::integer, 0)
                 else coalesce(oc.total_count::integer, 0)
             end                                   as total_count
@@ -2569,7 +2569,7 @@ VIEW_STRINGS = {
               sel3.sensor
             , sel3.timestamp_rounded
             , case
-                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB')     -- This filter applies to zone sensors for wich only the area_count is filled
+                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB', 'GABW')     -- This filter applies to zone sensors for wich only the area_count is filled
                 then coalesce(oc.area_count::integer, 0)
                 else coalesce(oc.total_count::integer, 0)
             end                                   as total_count
@@ -3023,7 +3023,7 @@ VIEW_STRINGS = {
               sel3.sensor
             , sel3.timestamp_rounded
             , case
-                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB')     -- This filter applies to zone sensors for wich only the area_count is filled
+                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB', 'GABW')     -- This filter applies to zone sensors for wich only the area_count is filled
                 then coalesce(oc.area_count::integer, 0)
                 else coalesce(oc.total_count::integer, 0)
               end                                   as total_count
@@ -3595,7 +3595,7 @@ VIEW_STRINGS = {
               sel3.sensor
             , sel3.timestamp_rounded
             , case
-                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB')     -- This filter applies to zone sensors for wich only the area_count is filled
+                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB', 'GABW')     -- This filter applies to zone sensors for wich only the area_count is filled
                 then coalesce(oc.area_count::integer, 0)
                 else coalesce(oc.total_count::integer, 0)
             end                                   as total_count
@@ -4140,7 +4140,7 @@ VIEW_STRINGS = {
               sel3.sensor
             , sel3.timestamp_rounded
             , case
-                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB')     -- This filter applies to zone sensors for wich only the area_count is filled 
+                when left(replace(sel3.sensor, 'CMSA-', ''), 4) in ('GADM', 'GAMM', 'GAAB', 'GABW')     -- This filter applies to zone sensors for wich only the area_count is filled 
                 then coalesce(oc.area_count::integer, 0)
                 else coalesce(oc.total_count::integer, 0)
             end                                   as total_count
