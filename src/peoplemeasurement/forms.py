f347fac63019e26fcbf26c577674158c23a14579
jjbeekman <jonathanjeroen@gmail.com>
2022-08-09 11:51:08 +0200

    Added validation error when no JSON is supplied and one of the other fields is blank

--- src/peoplemeasurement/forms.py
+++ src/peoplemeasurement/forms.py
@@ -6,34 +6,39 @@ from peoplemeasurement.models import Area, Line
 
 class BaseForm(forms.ModelForm):
     geom_type = "Undefined"
+    required_fields = []
 
     def clean(self):
         cleaned_data = super().clean()
         json_input = cleaned_data.get('json_input')
-        if json_input != 'null' and json_input:
+        if json_input:
             serializer_instance = self.serializer(data=json_input)
             if not serializer_instance.is_valid():
                 raise forms.ValidationError(' | '.join(serializer_instance.get_validation_errors()))
             if not json_input["geom"]["type"] == self.geom_type:
                 raise forms.ValidationError(f"The type of geom should be {self.geom_type}")
             cleaned_data["serializer_instance"] = serializer_instance
+        else:
+            for field in self.required_fields:
+                if not cleaned_data.get(field):
+                    raise forms.ValidationError(f"The field {field} cannot be empty when no JSON is supplied")
         return cleaned_data
 
     def save(self, commit=True):
         instance = super().save(commit=commit)
         json_input = self.cleaned_data.get('json_input')
-        if json_input and json_input != 'null':
+        if json_input:
             # There is json input, so we overwrite all fields with the info from the json
             serializer_instance = self.cleaned_data["serializer_instance"]
             serializer_instance.update(instance=instance, validated_data=serializer_instance.validated_data)
 
         return instance
 
-
 class LineForm(BaseForm):
     serializer = LineSerializer
+    required_fields = ['name', 'sensor', 'azimuth', 'geom']
     geom_type = "LineString"
-    json_input = forms.JSONField(required=False, help_text="""{
+    json_input = forms.JSONField(required=False, help_text="""<b>Example:</b> <pre>{
         "name": "test_name",
         "sensor": "sensor_123",
         "azimuth": 40,
@@ -44,7 +49,7 @@ class LineForm(BaseForm):
                 [52.3,4.9]
             ]
         }
-    }""")
+    }</pre>""")
 
     class Meta:
         model = Line
@@ -53,8 +58,9 @@ class LineForm(BaseForm):
 
 class AreaForm(BaseForm):
     serializer = AreaSerializer
+    required_fields = ['name', 'sensor', 'area', 'geom']
     geom_type = "Polygon"
-    json_input = forms.JSONField(required=False, help_text="""{
+    json_input = forms.JSONField(required=False, help_text="""<b>Example:</b> <pre>{
         "name": "test_name",
         "sensor": "sensor_123",
         "area": 40,
@@ -68,7 +74,7 @@ class AreaForm(BaseForm):
                 [52.3,4.8]
             ]]
         }
-    }""")
+    }</pre>""")
 
     class Meta:
         model = Area
