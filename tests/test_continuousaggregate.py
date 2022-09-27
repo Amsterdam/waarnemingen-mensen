a79361d4e0200fd65d46fc02cf3e5b2038505106
Hielke <kramerh@gmail.com>
2022-09-27 17:02:09 +0200

    Added a recalculate-history-since flag

--- tests/test_continuousaggregate.py
+++ tests/test_continuousaggregate.py
@@ -32,14 +32,14 @@ class TestDataIngressPoster:
         for i, sensor_name in enumerate(self.sensor_names):
             Sensors.objects.create(objectnummer=sensor_name, gid=i)
 
-    def test_vanilla(self, client):
+    def add_test_records(self, client):
         # Add records every 5min for multiple days
         Message.objects.all().delete()
         test_days = 2
         today = date.today()
-        start_date = today - timedelta(days=test_days)
+        start_date = today - timedelta(days=test_days-1)
         the_dt = datetime(start_date.year, start_date.month, start_date.day)
-        while the_dt < datetime(today.year, today.month, today.day):
+        while the_dt < (datetime(today.year, today.month, today.day)+timedelta(days=1)):
             for sensor_name in self.sensor_names:
                 test_post = json.loads(TEST_POST)
                 test_post['data'][0]['sensor'] = sensor_name
@@ -50,6 +50,11 @@ class TestDataIngressPoster:
         # Then run the parse_ingress script
         parser = TelcameraParser()
         parser.consume(end_at_empty_queue=True)
+
+        return today
+
+    def test_vanilla(self, client):
+        today = self.add_test_records(client)
 
         # Make sure we've got source data
         assert Observation.objects.all().count() > 100
@@ -62,9 +67,41 @@ class TestDataIngressPoster:
 
         # Take a record in the middle of the data in the continuous aggregate table
         # and check whether the record is made up of exactly 3 messages (one every 5 min)
-        last_record = Cmsa15Min.objects\
+        middle_record = Cmsa15Min.objects\
             .filter(sensor=self.sensor_names[0])\
             .filter(timestamp_rounded__gte=(today - timedelta(days=1)).isoformat())\
             .order_by('timestamp_rounded')\
             .first()
-        assert last_record.basedonxmessages == 3
+        assert middle_record.basedonxmessages == 3
+
+    def test_aggregate_recalculation(self, client):
+        today = self.add_test_records(client)
+
+        # Run the aggregator
+        call_man_command('complete_aggregate', 'continuousaggregate_cmsa15min')
+        # Check whether we have an aggregate for the last day
+        last_record = Cmsa15Min.objects \
+            .filter(sensor=self.sensor_names[0]) \
+            .order_by('-timestamp_rounded') \
+            .first()
+        assert today == last_record.timestamp_rounded.date()
+
+        # recalculate all history
+        call_man_command('complete_aggregate', 'continuousaggregate_cmsa15min', '--recalculate-history=true')
+        # Check again whether we have an aggregate for the last day
+        last_record = Cmsa15Min.objects \
+            .filter(sensor=self.sensor_names[0]) \
+            .order_by('-timestamp_rounded') \
+            .first()
+        assert today == last_record.timestamp_rounded.date()
+
+        # recalculate the history since yesterday
+        a_day_ago = (date.today() - timedelta(days=1)).isoformat()
+        call_man_command('complete_aggregate', 'continuousaggregate_cmsa15min',
+                         f'--recalculate-history-since={a_day_ago}')
+        # Check yet again whether we have an aggregate for the last day
+        last_record = Cmsa15Min.objects \
+            .filter(sensor=self.sensor_names[0]) \
+            .order_by('-timestamp_rounded') \
+            .first()
+        assert today == last_record.timestamp_rounded.date()
